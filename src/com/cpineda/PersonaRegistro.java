3df4c2055a7baa6a356afb099a0bdee6325756bc
pineda04 <cesar.pineda.assert@gmail.com>
2023-04-16 12:47:26 -0300

    Se agrego una linea inservible

--- src/com/cpineda/PersonaRegistro.java
+++ src/com/cpineda/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.cpineda;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
