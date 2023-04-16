caeaa299eb17e773df83977e9c7aa287635f2457
pineda04 <cesar.pineda.assert@gmail.com>
2023-04-16 13:05:13 -0300

    Se quito la lineda de codigo inservible

--- src/com/cpineda/PersonaRegistro.java
+++ src/com/cpineda/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.cpineda;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        // César esta linea de codigo se puede eliminar 
-        //System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
