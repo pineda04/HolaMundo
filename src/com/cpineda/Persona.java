4755947031f69dc788fad2d62a8b46f1b09f5908
pineda04 <cesar.pineda.assert@gmail.com>
2023-04-16 13:34:16 -0300

    Se reemplaza clase Persona por PersonaRegistro

--- src/com/cpineda/Persona.java
+++ src/com/cpineda/Persona.java
@@ -1,32 +1,8 @@
 package com.cpineda;
 
-public class Persona {
-    private String nombre, apellidos;
-
-    public Persona(String nombre, String apellidos) {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
+public record Persona(String nombre, String apellidos) {
     @Override
     public String toString() {
-        return String.format("%s %s", nombre, apellidos);
+        return "%s %s".formatted(nombre, apellidos);
     }
-
 }
