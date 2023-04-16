b8ba6a41ed4828e8521676efce4c1fd1c8ec3daf
Cesar <123453450+pineda04@users.noreply.github.com>
2023-04-16 12:58:30 -0300

    Comentario sobre codigo inservible

--- src/com/cpineda/PersonaRegistro.java
+++ src/com/cpineda/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com.cpineda;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("Esta instrucción es inservible");
+        // César esta linea de codigo se puede eliminar 
+        //System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
